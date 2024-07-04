195c23e80ad9e23c10c46975302af9be90b7a30c
CharrafiMed <mhch5003@gmail.com>
2024-07-04 19:28:42 +0200

    position setup

--- src/Customization/Highlighter.php
+++ src/Customization/Highlighter.php
@@ -1,12 +1,32 @@
 <?php
+
 namespace CharrafiMed\GlobalSearchModal\Customization;
 
 use Closure;
 
 
-class highlighter{
-    protected string $classes;
-    protected string $styles;
+class highlighter
+{
 
-    
+    protected string | array | Closure $classes;
+    protected string | array | Closure  $styles;
+
+    public function classes(string|array|Closure $classes): self
+    {
+        $this->classes = $classes;
+        return $this;
+    }
+    public function styles(string|array|Closure $styles): self
+    {
+        $this->styles = $styles;
+        return $this;
+    }
+    public function getClasses(): mixed
+    {
+        return $this->classes;
+    }
+    public function getStyles() 
+    {
+        return $this->styles;
+    }
 }
--- src/Customization/Position.php
+++ src/Customization/Position.php
@@ -2,6 +2,74 @@
 
 namespace CharrafiMed\GlobalSearchModal\Customization;
 
-class Position{
-    
+use Closure;
+
+class Position
+{
+    protected string $right;
+    protected string $top;
+    protected string $left;
+    protected string $bottom;
+    protected string $classes;
+    public function right(string|int|Closure $rightPosition, string $unit = null)
+    {
+        $this->right = $this->formatPosition($rightPosition, $unit);
+        return $this;
+    }
+
+    public function top(string|int|Closure $topPosition, string $unit = null)
+    {
+        $this->top = $this->formatPosition($topPosition, $unit);
+        return $this;
+    }
+
+    public function left(string|int|Closure $leftPosition, string $unit = null)
+    {
+        $this->left = $this->formatPosition($leftPosition, $unit);
+        return $this;
+    }
+
+    public function bottom(string|int|Closure $bottomPosition, string $unit = null)
+    {
+        $this->bottom = $this->formatPosition($bottomPosition, $unit);
+        return $this;
+    }
+    public function classes(string|int|Closure $classes)
+    {
+        $this->classes = $this->formatTailwindClasses($classes);
+        return $this;
+    }
+
+    protected function formatPosition(string|int|Closure $position, string $unit = null): string
+    {
+        if ($position instanceof Closure) {
+            $position = $position();
+        }
+
+        return is_null($unit) ? (string) $position : (string) $position . $unit;
+    }
+
+    protected function formatTailwindClasses(string|int|Closure $classes)
+    {
+    }
+
+    public function getRight()
+    {
+        return $this->right;
+    }
+
+    public function getTop()
+    {
+        return $this->top;
+    }
+
+    public function getLeft()
+    {
+        return $this->left;
+    }
+
+    public function getBottom()
+    {
+        return $this->bottom;
+    }
 }
