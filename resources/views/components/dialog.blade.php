0e987d099eddbec1cd90c586a15050b753a67df8
CharrafiMed <mhch5003@gmail.com>
2024-07-24 19:06:37 +0200

    make the modal work

--- resources/views/components/dialog.blade.php
+++ resources/views/components/dialog.blade.php
@@ -8,80 +8,43 @@
     // $isSlideOver = false;
     $isSlideOver = $this->getConfigs()->isSlideOver();
     $position = $this->getConfigs()->getPosition();
-    $top = $position?->getTop() ?: ($isSlideOver ? '0px'   : '40px');
+    $top = $position?->getTop() ?: ($isSlideOver ? '0px' : '40px');
     $left = $position?->getLeft() ?? '50%';
     $right = $position?->getRight() ?? '50%';
     $bottom = $position?->getBottom() ?? '50%';
- 
 
 @endphp
-<div 
-    @class(['flex justify-center']) 
-    x-ignore 
-    ax-load
+<div @class(['flex justify-center']) x-ignore ax-load
     ax-load-src="{{ FilamentAsset::getAlpineComponentSrc('global-search-modal-observer', 'charrafimed/global-search-modal') }}"
-    x-load-css="[@js(FilamentAsset::getStyleHref('global-search-modal', 'charrafimed/global-search-modal'))]" 
-    x-data="observer"
-    >
-    <div 
-        class="fixed inset-0 z-40 overflow-y-auto" 
-        role="dialog" 
-        aria-modal="true" 
-        style="display: none"
+    x-load-css="[@js(FilamentAsset::getStyleHref('global-search-modal', 'charrafimed/global-search-modal'))]" x-data="observer">
+    <div class="fixed inset-0 z-40 overflow-y-auto" role="dialog" aria-modal="true" style="display: none"
         x-show="$store.modalStore.open"
-        
-        @if ($isClosedByEscaping) 
-            x-on:keydown.escape.window="$store.modalStore.hideModal()" 
-        @endif
+        @if ($isClosedByEscaping) x-on:keydown.escape.window="$store.modalStore.hideModal()" @endif
+        x-id="['modal-title']" x-bind:aria-labelledby="$id('modal-title')">
 
-        x-id="['modal-title']" 
-        x-bind:aria-labelledby="$id('modal-title')">
-        
         <!-- Overlay -->
-        <div 
-            class="fixed inset-0 bg-black bg-opacity-50" 
-            x-show="$store.modalStore.open" 
-            x-transition.opacity>
+        <div class="fixed inset-0 bg-black bg-opacity-50" x-show="$store.modalStore.open" x-transition.opacity>
         </div>
-        
+
         <!-- Panel -->
         <div class="">
-            <div 
-            class="relative flex min-h-screen items-center justify-center p-4" 
-            x-show="$store.modalStore.open"
-            x-transition
+            <div class="relative flex min-h-screen items-center justify-center p-4" x-show="$store.modalStore.open"
+                x-transition @if ($isClosedByClickingAway) x-on:click="$store.modalStore.hideModal()" @endif>
 
-            @if ($isClosedByClickingAway)
-                x-on:click="$store.modalStore.hideModal()" 
-            @endif>
-
-            <div 
-            @class([
-                ' overflow-y-auto  bg-gray-800 py-1 px-0.5 shadow-lg',
-                'absolute inset-y-0 rounded-l-2xl right-0 max-w-sm w-full sm:w-1/2'=> $isSlideOver,
-                'absolute inset-x-0 top-20 w-full rounded-xl max-w-xl'=> !$isSlideOver,
-            ])
-        {{-- style="top: 0;right:0;left:0;bottom:0;" --}}
-        {{-- style="inset: 0" --}}
-            {{-- style=" top: {{ $top }};
-                        right: {{ $right }};
-                        left:  {{ $left }};
-                        bottom: {{ $bottom }}" --}}
-                
-                x-on:click.stop 
-                x-trap.noscroll.inert="$store.modalStore.open"
-                    >
-                    {{-- <x-global-search-modal::search.field />
-                
-                <div>
-                    @if ($results !== null)
-                    <x-global-search-modal::search.results
-                        :results="$results"
-                    />
-                @endif
-                </div> --}}
+                <div style=" top: 100px;" @class([
+                    'absolute bg-gray-800    py-1 px-0.5 shadow-lg',
+                    'inset-y-0 overflow-y-auto  rounded-l-2xl right-0 max-w-sm w-full sm:w-1/2' => $isSlideOver,
+                    'inset-x-0 border w-full rounded-xl max-w-xl mx-auto' => !$isSlideOver,
+                ]) x-on:click.stop
+                    x-trap.noscroll.inert="$store.modalStore.open">
+                    <x-global-search-modal::search.field />
+                    <div>
+                        @if ($results !== null)
+                            <x-global-search-modal::search.results :results="$results" />
+                        @endif
+                    </div>
+                </div>
             </div>
-        </div>
         </div>
     </div>
 </div>
