33761ab08cc072bd289669701d0d562397b4c81b
CharrafiMed <mhch5003@gmail.com>
2024-07-24 19:17:34 +0200

    wip

--- resources/views/components/modal/index.blade.php
+++ resources/views/components/modal/index.blade.php
@@ -1,38 +1,68 @@
+{{-- @php
+    use Filament\Support\Facades\FilamentAsset;
+    $debounce = filament()->getGlobalSearchDebounce();
+    $keyBindings = filament()->getGlobalSearchKeyBindings();
+    $suffix = filament()->getGlobalSearchFieldSuffix();
+    $isClosedByClickingAway = $this->getConfigs()->isClosedByClickingAway();
+    $isClosedByEscaping = $this->getConfigs()->isClosedByEscaping();
+    $isSlideOver = $this->getConfigs()->isSlideOver();
+    $position = $this->getConfigs()->getPosition();
+    $top = $position?->getTop() ?: ($isSlideOver ? '0px' : '40px');
+    $left = $position?->getLeft() ?? '50%';
+    $right = $position?->getRight() ?? '50%';
+    $bottom = $position?->getBottom() ?? '50%';
 
-<div x-data="{ open: false }" class="flex justify-center">
-    <!-- Trigger -->
-    <span x-on:click="open = true">
-        <button type="button" class="rounded-md bg-white px-5 py-2.5 shadow">
-            Open dialog
-        </button>
-    </span>
+@endphp --}}
 
-    <!-- Modal -->
-    <div
-        x-show="open"
-        style="display: none"
-        x-on:keydown.escape.prevent.stop="open = false"
-        role="dialog"
-        aria-modal="true"
-        x-id="['modal-title']"
-        :aria-labelledby="$id('modal-title')"
-        class="fixed inset-0 z-10 overflow-y-auto"
+@aware([
+    'debounce',
+    'keyBindings',
+    'suffix',
+    'isClosedByClickingAway',
+    'isClosedByEscaping',
+    'isSlideOver',
+    'top',
+    'left',
+    'right',
+    'bottom',
+])
+<div 
+    @class(['flex justify-center']) 
+    x-ignore 
+    ax-load
+    ax-load-src="{{ FilamentAsset::getAlpineComponentSrc('global-search-modal-observer', 'charrafimed/global-search-modal') }}"
+    x-load-css="[@js(FilamentAsset::getStyleHref('global-search-modal', 'charrafimed/global-search-modal'))]" 
+    x-data="observer"
     >
+    <div 
+        class="fixed inset-0 z-40 overflow-y-auto" 
+        role="dialog" 
+        aria-modal="true" 
+        style="display: none"
+        x-show="$store.modalStore.open"
+        @if ($isClosedByEscaping)
+             x-on:keydown.escape.window="$store.modalStore.hideModal()" 
+        @endif
+        x-id="['modal-title']" 
+        x-bind:aria-labelledby="$id('modal-title')">
+
         <!-- Overlay -->
-        <div x-show="open" x-transition.opacity class="fixed inset-0 bg-black bg-opacity-50"></div>
+        <div class="fixed inset-0 bg-black bg-opacity-50" x-show="$store.modalStore.open" x-transition.opacity>
+        </div>
 
         <!-- Panel -->
-        <div
-            x-show="open" x-transition
-            x-on:click="open = false"
-            class="relative flex min-h-screen items-center justify-center p-4"
-        >
-            <div
-                x-on:click.stop
-                x-trap.noscroll.inert="open"
-                class="relative w-full max-w-2xl overflow-y-auto rounded-xl bg-white p-12 shadow-lg"
-            >
-                
+        <div class="">
+            <div class="relative flex min-h-screen items-center justify-center p-4" x-show="$store.modalStore.open"
+                x-transition @if ($isClosedByClickingAway) x-on:click="$store.modalStore.hideModal()" @endif>
+
+                <div style=" top: 100px;" @class([
+                    'absolute bg-gray-800    py-1 px-0.5 shadow-lg',
+                    'inset-y-0 overflow-y-auto  rounded-l-2xl right-0 max-w-sm w-full sm:w-1/2' => $isSlideOver,
+                    'inset-x-0 border w-full rounded-xl max-w-xl mx-auto' => !$isSlideOver,
+                ]) x-on:click.stop
+                    x-trap.noscroll.inert="$store.modalStore.open">
+                    <slot />
+                </div>
             </div>
         </div>
     </div>
