010f5b4255bb6bed1930766fba1577bb5915d5cd
CharrafiMed <mhch5003@gmail.com>
2024-07-27 19:08:30 +0200

    feat: add styles and classes for highlighting feature

--- src/Concerns/CanHighlightQueryMatches.php
+++ src/Concerns/CanHighlightQueryMatches.php
@@ -7,12 +7,34 @@ trait CanHighlightQueryMatches
 
 {
     public  bool $highlightQueryMatches = true;
+    public  ?string $highlightQueryClasses;
+    public  ?string $highlightQueryStyles;
 
 
+    public  function highlightQueryStyles(?string $styles): self
+    {
+        $this->highlightQueryStyles = $styles;
+        return $this;
+    }
+    public  function highlightQueryClasses(?string $classes): self
+    {
+        $this->highlightQueryClasses = $classes;
+        return $this;
+    }
     public  function highlightQueryMatches(bool $enabled = true): self
     {
         $this->highlightQueryMatches = $enabled;
         return $this;
+    }
+
+    public function gethighlightQueryStyles()
+    {
+        return $this->highlightQueryStyles;
+    }
+
+    public function gethighlightQueryClasses()
+    {
+        return $this->highlightQueryClasses;
     }
 
     public  function isMustHighlightQueryMatches(): bool
