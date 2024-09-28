2d7fddb5b10f71b95b6b046e7285cb977dafb328
CharrafiMed <mhch5003@gmail.com>
2024-09-28 14:28:50 +0200

    wip

--- resources/views/components/icon/item-end-tree.blade.php
+++ resources/views/components/icon/item-end-tree.blade.php
@@ -1,4 +1,4 @@
-<svg class="w-6 h-12 stroke-2 rtl:rotate-y-90  text-gray-500 dark:text-gray-400 opacity-50" viewBox="0 0 24 54">
+<svg class="w-6 h-12 stroke-2 text-gray-500 rtl:rotate-y-180 dark:text-gray-400 opacity-50" viewBox="0 0 24 54">
     <g stroke="currentColor" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round">
         <path d="M8 6v21M20 27H8.3">
         </path>
