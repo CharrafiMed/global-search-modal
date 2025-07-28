1492dd04870a0f9bb6feae368a105a534cf8afde
charrafimed <mhch5003@gmail.com>
2025-07-28 19:33:20 +0200

    wip

--- resources/views/components/search/action-button.blade.php
+++ resources/views/components/search/action-button.blade.php
@@ -5,16 +5,6 @@
     'clickFunction' => null,
 ])
 
-<button
-    type="{{ $type }}"
-    title="{{ __($title) }}"
-    x-on:click.stop="{{ $clickFunction }}"
-    class="action-button  hover:bg-black/5 rounded-full cursor-pointer p-2 dark:hover:bg-white/10 duration transition"
-    >
-    {{-- todo :
-        refactor to dynamic components cz at this time does not seems to work and i don't know way
-     --}}
-</button>
 
 <button 
     data-action
@@ -22,7 +12,7 @@
     title="{{ $title }}"
     tabindex="1"
     x-on:click.stop="{{ $clickFunction }}"
-    {{ $attributes->merge(['class' => 'rounded-full relative cursor-pointer focus-visible:outline-none  focus:bg-gray-100 dark:focus:bg-white/10 border focus:border-gray-400 dark:focus:border-white/30 appearance-none rounded-full border-none bg-none p-1.5 text-gray-400 text-inherit dark:hover:bg-white/5 hover:bg-gray-800/5']) }}
+    {{ $attributes->merge(['class' => 'hover:bg-black/5 rounded-full cursor-pointer p-2 dark:hover:bg-white/10 duration transition relative cursor-pointer focus-visible:outline-none  focus:bg-gray-100 dark:focus:bg-white/10 border focus:border-gray-400 dark:focus:border-white/30 appearance-none rounded-full border-none bg-none p-1.5 text-gray-400 text-inherit dark:hover:bg-white/5 hover:bg-gray-800/5']) }}
 >
     <x-filament::icon :icon="$icon" class="bg-red-500"/>
 </button>
