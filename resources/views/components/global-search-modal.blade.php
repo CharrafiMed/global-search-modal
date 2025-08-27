1fb2ba6142c7e9268fcd81641cef3c4941a8cc08
charrafimed <mhch5003@gmail.com>
2025-08-27 23:43:28 +0200

    wip

--- resources/views/components/global-search-modal.blade.php
+++ resources/views/components/global-search-modal.blade.php
@@ -16,22 +16,22 @@
         // reduce also the padding of contents 
         '[&_.fi-modal-content]:!py-3 [&_.fi-modal-content]:!px-4', 
 
-        // WHY IS THE MODAL SO FAR AWAYYY?? come closer bb 💕 (reduce top padding a little bit)
+        // reduce top padding a little bit
         '[&_.fi-modal-window-ctn]:!grid-rows-[0.6fr_auto_1fr] [&_.fi-modal-window-ctn]:sm:!grid-rows-[0.5fr_auto_3fr]', 
   
         // give it some padding when the auto in "0.6fr_auto_1fr" expand across
         '[&_:not(.fi-modal-slide-over):not(.fi-width-screen)_.fi-modal-window-ctn]:!pt-16',
 
         // handle the close button 
-        '[&_.fi-modal-close-btn]:!top-2 [&_.fi-modal-close-btn]:!end-2 [&_.fi-modal-close-btn]:!p-0.5 [&_.fi-modal-close-btn]:size-6 ',
+        '[&_.fi-modal-close-btn]:!top-4 [&_.fi-modal-close-btn]:!end-4 [&_.fi-modal-close-btn]:!p-0.5 [&_.fi-modal-close-btn]:size-6 ',
         // control results container heights 
         '[&_:not(.fi-modal-slide-over):not(.fi-width-screen)_.results-container]:max-h-[67vh]', 
-        '[&_.fi-modal-slide-over_.results-container]:!max-h-[83vh]', 
-        '[&_.fi-width-screen_.results-container]:!max-h-[83vh]', 
+        '[&_.fi-modal-slide-over_.results-container]:!max-h-[80vh] [&_.fi-modal-slide-over_.results-container]:!min-h-full', 
+        '[&_.fi-width-screen_.results-container]:!max-h-[80vh]', 
 
         // chrome scroll bar sucks (make it looks like what firefox does)
         '[&_.results-container]:[scrollbar-width:thin]',
-        '[&_.results-container]:[scrollbar-color:rgba(156,163,175,0.7)_transparent]',
+        '[&_.results-container]:[scrollbar-color:rgba(156_,_163_,_175_,_0.7)_transparent]',
         '[&_.results-container::-webkit-scrollbar]:w-[6px]',
         '[&_.results-container::-webkit-scrollbar-thumb]:bg-gray-400/70',
         '[&_.results-container::-webkit-scrollbar-thumb]:rounded-full',
@@ -53,7 +53,7 @@
         openEventName='open-global-search-modal' 
         :attributes="new \Illuminate\View\ComponentAttributeBag([
             'width' => $modal->getWidth()?->value ?? Filament\Support\Enums\Width::TwoExtraLarge,
-            'closeButton' =>true,
+            'closeButton' => $modal->hasCloseButton(),
             'closedByClickingAway' => $modal->isClosedByClickingAway(),
             'closedByEscaping' => $modal->isClosedByEscaping(),
             'autofocus' => $modal->isAutofocus(),
